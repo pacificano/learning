7f229195332ee9e8aeb7b4116aa6a1c8bb6985e2
pacificano <tris.pacificano@gmail.com>
2016-09-16 15:11:29 +1000

    grabbing from flickr working

--- javascript jquery/Learning Web App Development/Chapter 5 The Bridge/flickr.js
+++ javascript jquery/Learning Web App Development/Chapter 5 The Bridge/flickr.js
@@ -1,12 +1,21 @@
 var main = function(){
 	"use strict";
 
-	// var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json";
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
 
 	$.getJSON(url, function(flickrResponse){
-		console.log(flickrResponse);
-	});
-};
+
+		// console.log(flickrResponse);
+
+		flickrResponse.items.forEach(function(photo){
+			var $img = $("<img>").hide();
+			$img.attr("src", photo.media.m);
+			$("main .photos").append($img);
+			$img.fadeIn();
+		});
+
+	}); // getJSON
+
+}; // main
 
 $(document).ready(main);
