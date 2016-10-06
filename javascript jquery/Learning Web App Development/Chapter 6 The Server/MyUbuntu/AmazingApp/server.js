c29136d7fde5e7e66c6010c81a2beb4ecd064122
pacificano <tris.pacificano@gmail.com>
2016-10-06 19:19:28 +1100

    amazing app installed

---
+++ javascript jquery/Learning Web App Development/Chapter 6 The Server/MyUbuntu/AmazingApp/server.js
@@ -0,0 +1,37 @@
+var express = require("express"),
+	http = require("http"),
+	app = express(),
+    toDos = [
+        { 
+            "description" : "Get groceries",
+            "tags"  : [ "shopping", "chores" ]
+        },
+        { 
+            "description" : "Make up some new ToDos",
+            "tags"  : [ "writing", "work" ]
+        },
+        {
+            "description" : "Prep for Monday's class",
+            "tags"  : [ "work", "teaching" ]
+        },
+        { 
+            "description" : "Answer emails",
+            "tags"  : [ "work" ]
+        },
+        { 
+            "description" : "Take Gracie to the park",
+            "tags"  : [ "chores", "pets" ]
+        },
+        { 
+            "description" : "Finish writing this book",
+            "tags"  : [ "writing", "work" ]
+        }
+    ]
+
+app.use(express.static(__dirname + "/client"));
+
+http.createServer(app).listen(3000);
+
+app.get("/todos.json", function (req, res){
+	res.json(toDos);
+});
