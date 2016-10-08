1a56f3fa93a8da4fb456bc4c6a98f3bfd3b9a4eb
pacificano <tris.pacificano@gmail.com>
2016-10-08 13:04:47 +1100

    connecting to redis

--- javascript jquery/Learning Web App Development/Chapter 7 The Data/My Ubuntu/Twitter/tweet_counter.js
+++ javascript jquery/Learning Web App Development/Chapter 7 The Data/My Ubuntu/Twitter/tweet_counter.js
@@ -9,24 +9,36 @@ twitter = ntwitter(credentials);
 
 client = redis.createClient();
 
-counts.awesome = 0;
-
-twitter.stream(
-	"statuses/filter", 
-	{ "track": ["awesome", "cool", "rad", "gnarly", "groovy"] },
-	function(stream){
-		stream.on("data", function(tweet){
-			if (tweet.text.indexOf("awesome") >= -1) {
-				client.incr("awesome");
-				counts.awesome = counts.awesome + 1;
-			}
-		});
+client.mget(["awesome", "cool"], function (err, awesomeCount) {
+	if (err !== null) {
+		console.log("ERROR: " + err);
+		return
 	}
-);
+
+	counts.awesome = parseInt(awesomeCount,10) || 0;
+	counts.cool = parseInt(awesomeCount,10) || 0;
+
+	twitter.stream(
+		"statuses/filter", 
+		{ "track": ["awesome", "cool", "rad", "gnarly", "groovy"] },
+		function(stream){
+			stream.on("data", function(tweet){
+				if (tweet.text.indexOf("awesome") >= -1) {
+					client.incr("awesome");
+					counts.awesome = counts.awesome + 1;
+				}
+			});
+		}
+	);
+
+});
+
+
 
 
 setInterval(function(){
 	console.log("awesome: " + counts.awesome);
+	console.log("cool: " + counts.cool);
 }, 3000);
 
 module.exports = counts;
