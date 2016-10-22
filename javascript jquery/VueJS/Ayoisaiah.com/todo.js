666904fca19660fb770f4dc539ed54a01ae88d5d
pacificano <tris.pacificano@gmail.com>
2016-10-22 11:55:56 +1100

    learning vue.js

---
+++ javascript jquery/VueJS/Ayoisaiah.com/todo.js
@@ -0,0 +1,17 @@
+// Vue JS Todo List
+
+// new Vue({
+// 	el: "#todo",
+// 	data: {
+// 		name: "Tristan"
+// 	},
+// 	methods: {}
+// });
+
+
+new Vue({
+  el: "#demo",
+  data: {
+    name: "Ayo Isaiah"
+  }
+});
