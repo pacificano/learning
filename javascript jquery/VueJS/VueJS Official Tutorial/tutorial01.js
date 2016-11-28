f2081d0ad11d77c9875f3ae2900b3c6e23c1ebb4
pacificano <tris.pacificano@gmail.com>
2016-11-28 13:19:48 +1100

    update vue js

--- javascript jquery/VueJS/VueJS Official Tutorial/tutorial01.js
+++ javascript jquery/VueJS/VueJS Official Tutorial/tutorial01.js
@@ -1,6 +1,28 @@
+// COMPONENTS
+Vue.component('todo-item', {
+	template: '<li>This is a todo item</li>'
+})
+
+Vue.component('custom-item',{
+	props: ['todo'],
+	template: '<li>{{ todo.text }}</li>'
+})
+
+// CREATE NEW VUE (has to come after the components)
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    groceryList: [{
+    	text: 'Vegetables'
+    },{
+    	text: 'Dogs'
+    },{
+    	text: 'Hats for summer'
+    }]
   }
 })
+
+
+
+
